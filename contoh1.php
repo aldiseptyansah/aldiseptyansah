23043a63cfdeed4e43dbabd0debfbb50af8528cc
aldiseptyansah <90689261+aldiseptyansah@users.noreply.github.com>
2021-09-21 14:31:37 +0700

    Add files via upload

---
+++ contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh1 extends CI_controller
+{
+        public function index()
+        {
+            echo "<h1>perkenalkan</h1>";
+            echo "nama saya aldi septyansah
+                saya berasal dari palembang
+                saya menyukai olahraga futsal";
+        }
+    }
