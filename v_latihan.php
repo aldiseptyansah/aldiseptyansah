f561afa803de9043e96229a239a38fa3dc5b5104
aldiseptyansah <90689261+aldiseptyansah@users.noreply.github.com>
2021-09-23 19:32:22 +0700

    menambahkan views di pertemuan 2

---
+++ v_latihan.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Latihan 1</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
+	<style type="text/css">
+		body {
+
+			background: linear-gradient(#FF3333, #33FF92);
+			height: 70vh;
+		}
+
+		.home {
+			display: flex;
+			align-items: center;
+			justify-content: center;
+
+			margin-top: 230px;
+		}
+	</style>
+</head>
+
+<body>
+	<div class="home">
+		<div class="alert alert-danger" role="alert">
+			<h4 class="alert-heading">Halo Kawan.. Yuk kita belajar web pemrograman codeigniter..!!</h4>
+			<p>
